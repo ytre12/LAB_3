8817a75585ecb4b608bb778faabe751e3016306b
ytre12 <obio12yt@gmail.com>
2025-11-30 22:13:17 +0200

    Add files via upload

--- LAB3.js
+++ LAB3.js
@@ -2,6 +2,8 @@ const xValueElement = document.querySelector("#xValue");
 const yValueElement = document.querySelector("#yValue");
 const iterationElement = document.querySelector("#Iteration");
 
+const btnYacobiMethodElement = document.getElementById("JacobiMetod");
+const btnEasyIterMethodElement = document.getElementById("EasyIterMethod");
 
 const formX = (x, y) => {
     return (Math.sin(x + y) + 0.1) / 1.5;
@@ -19,9 +21,40 @@ const detector = (curX, curY, nextX, nextY) => {
 }
 
 const E = 0.0001;
-let iteration = 0;
+
+const EasyIterationMethod = () => {
+    let iteration = 0;
+
+    let curX = 1;
+    let curY = -1;
+
+    let nextX = formX(curX, curY);
+    let nextY = formY(curX, curY);
+
+    while(detector(curX, curY, nextX, nextY)) {
+        
+        curX = nextX;
+        curY = nextY;
+
+        nextX = formX(curX, curY);
+        nextY = formY(nextX, curY);
+        
+        iteration++;
+    }
+
+    console.log(nextX);
+    console.log(nextY);
+    console.log(iteration);
+
+    xValueElement.textContent = `x ≈ ${nextX}`;
+    yValueElement.textContent = `y ≈ ${nextY}`;
+    iterationElement.textContent = `Iteration = ${iteration}`;
+} 
+
 
 const yakobiMethod = () => {
+    let iteration = 0;
+
     let curX = 1;
     let curY = -1;
 
@@ -49,3 +82,11 @@ const yakobiMethod = () => {
 } 
 
 yakobiMethod();
+
+btnEasyIterMethodElement.onclick = () => {
+    EasyIterationMethod();
+}
+
+btnYacobiMethodElement.onclick = () => {
+    yakobiMethod();
+}
